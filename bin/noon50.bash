50f5571932addb453b6c7350de84f7472354125c
Dan Bikle <dan.bikle@gmail.com>
2015-12-19 00:01:01 +0000

    hello

---
+++ bin/noon50.bash
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# ~/pyspy/bin/noon50.bash
+# This should be run from cron with an entry like this:
+# 50 12 * * mon,tue,wed,thu,fri ${HOME}/pyspy/bin/noon50.bash > /tmp/noon50_bash.txt 2>&1
+#
+echo '$HOME is '$HOME
+date
+echo bye
