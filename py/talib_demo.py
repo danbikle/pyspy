8a442031e7c3b2e93fe518632a41d0f8c7c8fea2
Dan Bikle <dan.bikle@gmail.com>
2015-12-21 17:48:02 +0000

    more

--- py/talib_demo.py
+++ py/talib_demo.py
@@ -15,7 +15,7 @@ import sys
 #  len(sys.argv) should == 2
 if len(sys.argv) == 1:
   print('Demo:')
-  print('cd ~/ddata')
+  print('cd ~/pyspy/demodata')
   print('python ~/pyspy/py/talib_demo.py ftrGSPC2.csv')
   print('...')
   sys.exit()
@@ -24,7 +24,7 @@ infile = sys.argv[1]
 df1    = pd.read_csv(infile)
 cp_a   = df1['cp'].values
 
-output = talib.SMA(cp_a)
+sma_a = talib.SMA(cp_a)
 # help(talib.SMA)
 # http://mrjbq7.github.io/ta-lib/func.html
 # http://mrjbq7.github.io/ta-lib/func_groups/overlap_studies.html
@@ -32,13 +32,13 @@ output = talib.SMA(cp_a)
 
 from talib import MA_Type
 # Calculating bollinger bands, with triple exponential moving average:
-upper, middle, lower = talib.BBANDS(cp_a, matype=MA_Type.T3)
+upper_a, middle_a, lower_a = talib.BBANDS(cp_a, matype=MA_Type.T3)
 # help(talib.BBANDS)
 # help(MA_Type)
 # http://www.google.com/search?q=in+technical+analysis+what+are+bollinger+bands
 # http://www.tadoc.org/indicator/BBANDS.htm
 
-mom_l = talib.MOM(cp_a, timeperiod=5)
+mom_a = talib.MOM(cp_a, timeperiod=5)
 # help(talib.MOM)
 # http://www.google.com/search?q=in+technical+analysis+what+is+momentum+indicator
 # http://www.tadoc.org/indicator/MOM.htm
@@ -47,6 +47,7 @@ mom_l = talib.MOM(cp_a, timeperiod=5)
 
 from talib.abstract import *
 # http://mrjbq7.github.io/ta-lib/abstract.html
-sma_l = SMA(df1, timeperiod=25, price='cp')
+sma_df = SMA(df1, timeperiod=25, price='cp')
 
+'end'
 
