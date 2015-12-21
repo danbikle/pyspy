d16fcf385831ac44b76a9f76765b6a73dfe1b9f1
Dan Bikle <dan.bikle@gmail.com>
2015-12-21 17:56:40 +0000

    more

--- py/talib_demo.py
+++ py/talib_demo.py
@@ -24,21 +24,31 @@ infile = sys.argv[1]
 df1    = pd.read_csv(infile)
 cp_a   = df1['cp'].values
 
-sma_a = talib.SMA(cp_a)
+sma_a  = talib.SMA(cp_a)
+
 # help(talib.SMA)
 # http://mrjbq7.github.io/ta-lib/func.html
 # http://mrjbq7.github.io/ta-lib/func_groups/overlap_studies.html
 # http://www.tadoc.org/indicator/SMA.htm
 
+# For convenience, the Function API supports both numpy.ndarray and pandas.Series types?
+# sma_df = talib.SMA(df1['cp'])
+# *** TypeError: Argument 'real' has incorrect type (expected numpy.ndarray, got Series)
+# talib.SMA() supports only numpy.ndarray
+
 from talib import MA_Type
+
 # Calculating bollinger bands, with triple exponential moving average:
+
 upper_a, middle_a, lower_a = talib.BBANDS(cp_a, matype=MA_Type.T3)
+
 # help(talib.BBANDS)
 # help(MA_Type)
 # http://www.google.com/search?q=in+technical+analysis+what+are+bollinger+bands
 # http://www.tadoc.org/indicator/BBANDS.htm
 
 mom_a = talib.MOM(cp_a, timeperiod=5)
+
 # help(talib.MOM)
 # http://www.google.com/search?q=in+technical+analysis+what+is+momentum+indicator
 # http://www.tadoc.org/indicator/MOM.htm
@@ -46,8 +56,10 @@ mom_a = talib.MOM(cp_a, timeperiod=5)
 # Demo of abstract
 
 from talib.abstract import *
+
 # http://mrjbq7.github.io/ta-lib/abstract.html
-sma_df = SMA(df1, timeperiod=25, price='cp')
+
+sma2_df = SMA(df1, timeperiod=25, price='cp')
 
 'end'
 
