82830a32c00ae731a10fb0090887dcc2cfef8096
Dan Bikle <dan.bikle@gmail.com>
2015-12-21 19:32:59 +0000

    more

--- py/talib_demo.py
+++ py/talib_demo.py
@@ -12,16 +12,7 @@ import pdb
 # I should check cmd line arg
 import sys
 
-#  len(sys.argv) should == 2
-if len(sys.argv) == 1:
-  print('Demo:')
-  print('cd ~/pyspy/demodata')
-  print('python ~/pyspy/py/talib_demo.py ftrGSPC2.csv')
-  print('...')
-  sys.exit()
-
-infile = sys.argv[1]
-df1    = pd.read_csv(infile)
+df1    = pd.read_csv('ftrGSPC2.csv')
 cp_a   = df1['cp'].values
 
 sma_a  = talib.SMA(cp_a)
@@ -55,11 +46,34 @@ mom_a = talib.MOM(cp_a, timeperiod=5)
 # http://mrjbq7.github.io/ta-lib/func_groups/momentum_indicators.html
 # Demo of abstract
 
-from talib.abstract import *
+# demos using abstract API:
+
+from talib import abstract
 
 # http://mrjbq7.github.io/ta-lib/abstract.html
 
-sma2_df = SMA(df1, timeperiod=25, price='cp')
+# Notice that first param is my dataframe named df1:
 
+sma2_df = abstract.SMA(df1, timeperiod=25, price='cp')
+
+# upperband_df, middleband_df, lowerband_df = abstract.BBANDS(price=close, timeperiod=5, nbdevup=2, nbdevdn=2, matype=0)
+
+df2 = pd.read_csv('GSPC.csv')
+cdate_l  = list(reversed(df2['Date'  ].values))
+open_l   = list(reversed(df2['Open'  ].values))
+high_l   = list(reversed(df2['High'  ].values))
+low_l    = list(reversed(df2['Low'   ].values))
+close_l  = list(reversed(df2['Close' ].values))
+volume_l = list(reversed(df2['Volume'].values))
+
+df3 = pd.DataFrame(cdate_l)
+df3.columns   = ['cdate']
+df3['open']   = open_l
+df3['high']   = high_l
+df3['low']    = low_l
+df3['close']  = close_l
+df3['volume'] = volume_l
+pdb.set_trace()
+df3.tail()
 'end'
 
