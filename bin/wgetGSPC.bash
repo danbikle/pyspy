56f93ab7a3c828ca1a3681eb5e626a22ed42f335
Dan Bikle <dan.bikle@gmail.com>
2015-12-22 21:26:23 +0000

    more

--- bin/wgetGSPC.bash
+++ bin/wgetGSPC.bash
@@ -16,7 +16,7 @@ wget --output-document=${TKR}.csv http://ichart.finance.yahoo.com/table.csv?s=${
 cat ${TKR}.csv | awk -F, '{print $1 "," $5}' > ${TKR}2.csv
 wget --output-document=${TKR}.html http://finance.yahoo.com/q?s=$TKRH
 # I should extract recent prices from html
-python ~pyspy/py/extprice.py
+python ${PYSPY}/py/extprice.py
 # I should cat prices together
 echo 'cdate,cp'                                > ${TKR}3.csv
 cat ${TKR}recent.csv ${TKR}2.csv|grep -v Date >> ${TKR}3.csv
