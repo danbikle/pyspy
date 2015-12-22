deba64e76108bd4b8c62fc72560242ed37d0d360
Dan Bikle <dan.bikle@gmail.com>
2015-12-22 09:28:00 +0000

    more

--- py/learn_test.py
+++ py/learn_test.py
@@ -79,6 +79,42 @@ lead_l[len(lead_l)-1] == lead_l[len(lead_l)-2] # should be true
 lead_a = np.array(lead_l) - np.array(cp_l)
 test_df['lead_delta'] = list(lead_a)
 test_df['actual_dir'] = [np.sign(lead_delta) for lead_delta in lead_a]
-pdb.set_trace()
+
+# I should count positive predictions.
+posp_df  = test_df[['pdir','actual_dir']][test_df['pdir'] == 1]
+# I should count true positive predictions.
+tposp_df = posp_df[posp_df['actual_dir'] == 1]
+# I should calculate positive accuracy.
+pos_acc  = 100.0 * len(tposp_df)/len(posp_df)
+
+# I should count negative predictions.
+negp_df  = test_df[['pdir','actual_dir']][test_df['pdir'] == -1]
+# I should count true negative predictions.
+tnegp_df = negp_df[negp_df['actual_dir'] == -1]
+# I should calculate negative accuracy.
+neg_acc  = 100.0 * len(tnegp_df)/len(negp_df)
+
+# I should calculate combined accuracy
+com_acc  = 100.0 * (len(tposp_df)+len(tnegp_df))/len(test_df)
+
+# I should calculate 'effectiveness'.
+pos_pctlead_a = np.array(test_df[['pctlead']][test_df['pdir'] == 1])
+pos_eff       = np.mean(pos_pctlead_a)
+neg_pctlead_a = np.array(test_df[['pctlead']][test_df['pdir'] == -1])
+neg_eff       = np.mean(neg_pctlead_a)
+
+# I should report
+print('True  positive count: '+str(len(tposp_df)))
+print('False positive count: '+str(len(posp_df)-len(tposp_df)))
+
+print('True  negative count: '+str(len(tnegp_df)))
+print('False negative count: '+str(len(negp_df)-len(tnegp_df)))
+
+print('Accuracy of positive predictions: ' +str(pos_acc)[:6]+'%')
+print('Accuracy of negative predictions: ' +str(neg_acc)[:6]+'%')
+print('Combined Accuracy of predictions: ' +str(com_acc)[:6]+'%')
+print('Positive Effectiveness: ' +str(pos_eff)[:6]+'%')
+print('Negative Effectiveness: ' +str(neg_eff)[:6]+'%')
+
 'done'
 
