a1b375e8abbb74d247b8cb0fcabf10333daf7743
Dan Bikle <dan.bikle@gmail.com>
2015-12-22 18:44:31 +0000

    more

--- py/learn_test.py
+++ py/learn_test.py
@@ -102,19 +102,19 @@ pos_pctlead_a = np.array(test_df[['pctlead']][test_df['pdir'] == 1])
 pos_eff       = np.mean(pos_pctlead_a)
 neg_pctlead_a = np.array(test_df[['pctlead']][test_df['pdir'] == -1])
 neg_eff       = np.mean(neg_pctlead_a)
-
+longonly_eff  = np.mean(test_df['pctlead'].values)
 # I should report
 print('True  positive count: '+str(len(tposp_df)))
 print('False positive count: '+str(len(posp_df)-len(tposp_df)))
-
 print('True  negative count: '+str(len(tnegp_df)))
 print('False negative count: '+str(len(negp_df)-len(tnegp_df)))
 
 print('Accuracy of positive predictions: ' +str(pos_acc)[:6]+'%')
 print('Accuracy of negative predictions: ' +str(neg_acc)[:6]+'%')
 print('Combined Accuracy of predictions: ' +str(com_acc)[:6]+'%')
-print('Positive Effectiveness: ' +str(pos_eff)[:6]+'%')
-print('Negative Effectiveness: ' +str(neg_eff)[:6]+'%')
+print('Positive Effectiveness: '  +str(pos_eff)[:6]+'%')
+print('Negative Effectiveness: '  +str(neg_eff)[:6]+'%')
+print('Long Only Effectiveness: ' +str(longonly_eff)[:6]+'%')
 
 'done'
 
