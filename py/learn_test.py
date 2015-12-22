0c8fe6e6113c5b5db0b8596bee5bb8f69113852f
Dan Bikle <dan.bikle@gmail.com>
2015-12-22 07:14:53 +0000

    more

--- py/learn_test.py
+++ py/learn_test.py
@@ -21,7 +21,24 @@ if len(sys.argv) < 3:
   print('...')
   sys.exit()
 
-pdb.set_trace()
 trainf = sys.argv[1]
 testf  = sys.argv[2]
+
+# I should load the CSV files
+train_df = pd.read_csv(trainf)
+test_df  = pd.read_csv(testf)
+train_a = np.array(train_df)
+test_a  = np.array(test_df)
+
+# I should declare some integers to help me navigate the Arrays.
+pdb.set_trace()
+cdate_i   = 0
+cp_i      = 1
+pctlead_i = 2
+pctlag1_i = 3
+pctlag2_i = 4
+pctlag4_i = 5
+pctlag8_i = 6
+upf_i     = 7
+lowf_i    = 8
 
