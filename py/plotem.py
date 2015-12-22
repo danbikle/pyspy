8d5bf4ccdb6d0efa7f08653d12dbf0be77adb92e
Dan Bikle <dan.bikle@gmail.com>
2015-12-22 20:22:28 +0000

    more

--- py/plotem.py
+++ py/plotem.py
@@ -30,7 +30,19 @@ print(csvf)
 # I should load the csv into a DataFrame
 df1 = pd.read_csv(csvf)
 
+# matplotlib likes dates:
+cdate_l = [datetime.datetime.strptime(row, "%Y-%m-%d") for row in df1['cdate'].values]
+# I should get values for y-axis now:
+cp_l = [row for row in df1['cp']       ] 
+gl_l = [row for row in df1['greenline']] 
 
+# I should plot
+import matplotlib
+# http://matplotlib.org/faq/howto_faq.html#generate-images-without-having-a-window-appear
+matplotlib.use('Agg')
+# Order is important here.
+# Do not move the next import:
+import matplotlib.pyplot as plt
 
 pdb.set_trace()
 'done'
