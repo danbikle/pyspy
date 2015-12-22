7213a8cd975c3b89d68352b106b559cd02fe059a
Dan Bikle <dan.bikle@gmail.com>
2015-12-22 06:37:00 +0000

    more

--- py/gen_train.py
+++ py/gen_train.py
@@ -25,8 +25,14 @@ yr1    = sys.argv[1]
 yr2    = sys.argv[2]
 infile = sys.argv[3]
 
-# I should load the csv into a DF.
-pdb.set_trace()
+# I should load the CSV into a DF.
 df1 = pd.read_csv(infile)
-df2 = df1[df1.columns]['cdate'>yr1]
-df3 = df2[df1.columns]['cdate'<yr2]
+# I should remove older data I do not want:
+pred1 = df1['cdate']>yr1
+df2   = df1[pred1]
+# I should remove newer data which I will test with later:
+pred2 = df2['cdate']<yr2
+df3   = df2[pred2]
+# I should write it to CSV
+df3.to_csv('training.csv', float_format='%4.3f', index=False)
+'done'
